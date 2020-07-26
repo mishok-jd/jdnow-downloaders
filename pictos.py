badfba675ba19d289049a739813c2c65ea42aa8b
mishok-jd <68820077+mishok-jd@users.noreply.github.com>
2020-07-26 21:33:44 +0500

    Update pictos.py

--- pictos.py
+++ pictos.py
@@ -10,6 +10,9 @@ pictos_png = 'http://jdnow-api-contentapistoragest.justdancenow.com/songs/' + co
 pictos_json = 'http://jdnow-api-contentapistoragest.justdancenow.com/songs/' + codename + '/assets/web/pictos-atlas.json'
 
 #download links
-webbrowser.open(pictos_png)
-webbrowser.open(pictos_json)
-print('Finished!')
+if codename == '':
+    print('ERROR: 1')
+else :
+    webbrowser.open(pictos_png)
+    webbrowser.open(pictos_json)
+    print('Finished!')
